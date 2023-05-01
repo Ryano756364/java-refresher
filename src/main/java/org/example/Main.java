1a754743486c9b301f5a7737524d5092d6d09d4a
rodell <ryanodell2668@gmail.com>
2023-05-01 19:00:35 -0400

    Homework Question 2 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,29 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int score;  //this is a named memory location called score of data type int
+        double a;
+        double b;
+        double c;
+        double answer;
+        boolean result = false;
 
-        score = 3;
+        a = 10.0;
+        b =  3.0;
+        c = 14.0;
+        answer = 0.0;
 
-        System.out.println("Score is " + score);
+        answer = a * b + (2 * b);
+
+        result = b < a || a > 14;
+
+        boolean is_True = result;
+
+        System.out.println(result);  //true
+        System.out.println(is_True);  //true
+
+        result = false;
+        System.out.println(result);  //false
+        System.out.println(is_True);  //still true because storing original memory address; not value of result
+
     }
 }
