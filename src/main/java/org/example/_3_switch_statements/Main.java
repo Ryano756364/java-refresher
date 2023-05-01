3fc39e9c7fad71dd8e473fed225f8690f0211d92
rodell <ryanodell2668@gmail.com>
2023-05-01 19:25:50 -0400

    Switch syntax complete

---
+++ src/main/java/org/example/_3_switch_statements/Main.java
@@ -0,0 +1,29 @@
+package org.example._3_switch_statements;
+
+public class Main {
+
+    //Switch statements are usually used in menu-type of selections
+
+    public static void main(String[] args) {
+        int a = 3;  //plan is to compare to a and if nothing is satisfied, use the default switch output
+
+        char ch = 'y';
+
+        String name = "Ann";
+
+        switch (a){
+            case 1:
+                System.out.println("a has a value of 1");
+                break;  //DON'T FORGET THESE! VERY IMPORTANT!
+            case 2:
+                System.out.println("a has a value of 2");
+                break;
+            case 3:
+                System.out.println("a has a value of 3");
+                break;
+            default:
+                System.out.println("the value of a is not 1, 2, or 3");
+        }
+    }
+
+}
