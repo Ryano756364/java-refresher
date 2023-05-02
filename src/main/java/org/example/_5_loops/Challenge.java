aada5fc33f17eae644b7967fc52cd293f7e35b84
rodell <ryanodell2668@gmail.com>
2023-05-02 08:25:17 -0400

    Loop challenges complete

--- src/main/java/org/example/_5_loops/Challenge.java
+++ src/main/java/org/example/_5_loops/Challenge.java
@@ -8,6 +8,15 @@ public class Challenge {
 
         //Question #2
         questionTwoDayTwo();
+
+        //Question #3
+        questionThreeDayTwo();
+
+        //Question #4
+        questionFourDayTwo();
+
+        //Question #6
+        questionSixDayTwo();
     }
 
     public static void questionOneDayTwo(){
@@ -53,4 +62,32 @@ public class Challenge {
                 break;
         }
     }
+
+    public static void questionThreeDayTwo(){
+        int x = 100;
+        System.out.println(x);
+        do {
+            x -= 5;
+            System.out.println(x);
+        } while (x > 0);
+    }
+
+    public static void questionFourDayTwo(){
+        char a = 'a', b = 'b', c = 'c';
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(a + " " + b + " " + c);
+        }
+    }
+
+    public static void questionSixDayTwo(){
+        double x = 40.0, y = 60.0;
+        System.out.println("x=" + x + " y=" + y);
+
+        double holder = x;
+        x = y;
+        y = holder;
+        System.out.println("x=" + x + " y=" + y);
+    }
+
 }
